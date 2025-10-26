e03fec88491dbe2fa4933497f40c2716209f53d3
ponddevJR <663170010324@rmu.ac.th>
2025-10-26 16:10:14 +0700

    add projects path

---
+++ app/projects/page.jsx
@@ -0,0 +1,11 @@
+const Projects = () => {
+  return (
+    <div className="w-full h-screen flex flex-col p-20 bg-gradient-to-br from-gray-800 to-black items-center justify-center">
+      <p className="text-center text-3xl font-bold text-gray-100">
+        If you seeing this,I'm working on it and the update will be very soon.<br/>
+        Thank you for visiting Pondfolio!
+      </p>
+    </div>
+  );
+};
+export default Projects;
